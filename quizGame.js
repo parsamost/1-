ed06aee6597862e9d86bde2e756a558a93d69770
parsamost <138319615+parsamost@users.noreply.github.com>
2023-11-13 22:11:56 +0330

    new project

---
+++ quizGame.js
@@ -0,0 +1,40 @@
+//const prompt = require("prompt-sync")();
+/*
+console.log("Welcome to the GAME!");
+
+let correctAnswers = 0;
+const totalQuestions = 3;
+
+const answer = prompt("what is the computer?");
+const correctAnswer = "CPU";
+
+if (answer.toUpperCase() === correctAnswer) {
+  console.log("thats correct!");
+  correctAnswers++;
+} else {
+  console.log("its wrong.")
+}
+
+const answer2 = prompt("what is better 3090 ot 4060ti?");
+const correctAnswer2 = "4060ti";
+
+if (answer2.toLowerCase() === correctAnswer2) {
+  console.log("thats correct!");
+  correctAnswers++;
+} else {
+  console.log("its wrong.")
+}
+
+const answer3 = prompt("what is the reccomended amount of RAM in 2023?");
+const correctAnswer3 = "32GB";
+
+if (answer.toUpperCase() === correctAnswer) {
+  console.log("thats correct!");
+  correctAnswers++;
+} else {
+  console.log("its wrong.")
+}
+
+const scoreByPercent = Math.round((correctAnswers / totalQuestions)*100)
+console.log("you got", correctAnswers, "questions correct!");
+console.log("you score", scoreByPercent.toString(),"%!")*/
